df5185e4abd4d10c5adb023f11027065330ab25a
Chadrack Mbala <102918698+chadrackmbala@users.noreply.github.com>
2023-07-11 17:02:36 +0100

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,45 @@
+// let nombre = 10;
+// 
+
+// let nombre = 10;
+// let partie = 'a';
+
+// for(i = 10; i <= nombre + 10; i++) {
+//     if (partie === 'a') {
+//         if (nombre % 2 === 0) {
+//             console.log(`Le nombre ${nombre} est paire`);
+//         } else {
+//             console.log('Le nombre n\'est pas paire');
+//         }
+//     } else {
+//         console.log('Le nombre n\'est pas reconu');
+//     }
+// }
+
+// function calculLaSomme (n, partie) {
+//     if(partie) {
+        
+//     }
+    
+    
+    
+    
+//     // if (partie === 'a' && n % 2 ) {
+        
+//     // }
+// }
+
+// console.log(calculLaSomme(6, 'a'));
+
+let somme;
+let nombre = 7;
+
+for(let compteur = 10; compteur <= nombre + 8; compteur++) {
+    if(nombre % 2 === 0) {
+        console.log(compteur);
+    } else {
+        console.log('Fin')
+    }
+    console.log(nombre);
+    
+}
