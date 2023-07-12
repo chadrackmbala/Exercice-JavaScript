e82f80a2d47d290f3460137f5445a083b095a2b9
Chadrack Mbala <102918698+chadrackmbala@users.noreply.github.com>
2023-07-12 03:48:43 +0100

    Add files via upload

--- app3.js
+++ app3.js
@@ -83,36 +83,137 @@
 
   // nParite(6, 'a')
 }
+{
+  // function produitDeDeuxNombres(a, b) {
+  //   let produit = a - b;
+  //   switch (produit) {
+  //     case produit > 0:
+  //       console.log(1);
+  //       break;
+  //     case produit === 0:
+  //       console.log(0);
+  //       break;
+  //     // case produit < 0:
+  //     //   console.log(-1);
+  //     //   break;
+  //   //   default:
+  //   //     console.log(0)
+  //   // }
+  // }
 
-// function produitDeDeuxNombres(a, b) {
-//   let produit = a - b;
-//   switch (produit) {
-//     case produit > 0:
-//       console.log(1);
-//       break;
-//     case produit === 0:
-//       console.log(0);
-//       break;
-//     // case produit < 0:
-//     //   console.log(-1);
-//     //   break;
-//   //   default:
-//   //     console.log(0)
-//   // }
-// }
+  // produitDeDeuxNombres(8,6)
+}
+{
+  // function productSign(n, m) {
+  //   n = n - m;
+  //   if (n > 0) {
+  //     console.log(n = 1);
+  //   } else if (n === 0) {
+  //     console.log(n = 0);
+  //   } else {
+  //     console.log(n = -1);
+  //   }
+  //   return n
+  // }
 
-// produitDeDeuxNombres(8,6)
+  // productSign(6, 6)
+}
+{
+  //   let tableauNombres = [];
+  // let tableauNombresMultpleDe3 = [];
+  // let tableauNombresMultpleDe5 = [];
+  // let sommeNombrePour3 = 0;
+  // let sommeNombrePour5 = 0;
 
-function productSign(n, m) {
-  n = n - m;
-  if (n > 0) {
-    console.log(n = 1);
-  } else if (n === 0) {
-    console.log(n = 0);
-  } else {
-    console.log(n = -1);
-  }
-  return n
+  // for(i = 3; i < 23; i++) {
+  //   let nombre = i;
+  //   if(nombre % 3 === 0 || nombre % 5 === 0) {
+  //     tableauNombres.push(i);
+  //   }
+  //   if(nombre % 3 === 0) {
+  //     tableauNombresMultpleDe3.push(i)
+  //   }
+  //   if(nombre % 5 === 0) {
+  //     tableauNombresMultpleDe5.push(i)
+  //   } else {
+  //     console.log(`${nombre} n'est ni multiple de 3, ni multiple de 5`);
+  //   }
+  // }
+
+  // for(i = 0; i < tableauNombresMultpleDe3.length ; i++) {
+  //   sommeNombrePour3 = sommeNombrePour3 + tableauNombresMultpleDe3[i];
+  // }
+
+  // for(i = 0; i < tableauNombresMultpleDe5.length ; i++) {
+  //   sommeNombrePour5 = sommeNombrePour5 + tableauNombresMultpleDe5[i];
+  // }
+  // console.log(tableauNombres);
+  // console.log(tableauNombresMultpleDe3);
+  // console.log(tableauNombresMultpleDe5);
+  // console.log(sommeNombrePour3);
+  // console.log(sommeNombrePour5);
 }
 
-productSign(3, 6)
+{
+  // function multiple_3_5(n) {
+  //   let tableauNombres = [];
+  //   let tableauNombresMultpleDe3 = [];
+  //   let tableauNombresMultpleDe5 = [];
+  //   let sommeNombrePour3 = 0;
+  //   let sommeNombrePour5 = 0;
+
+  //   for (i = 3; i < 23; i++) {
+  //     let nombre = i;
+  //     if (nombre % 3 === 0 || nombre % 5 === 0) {
+  //       tableauNombres.push(i);
+  //     }
+  //     if (nombre % 3 === 0) {
+  //       tableauNombresMultpleDe3.push(i)
+  //     }
+  //     if (nombre % 5 === 0) {
+  //       tableauNombresMultpleDe5.push(i)
+  //     }
+  //   }
+
+  //   for (i = 0; i < tableauNombresMultpleDe3.length; i++) {
+  //     sommeNombrePour3 = sommeNombrePour3 + tableauNombresMultpleDe3[i];
+  //   }
+
+  //   for (i = 0; i < tableauNombresMultpleDe5.length; i++) {
+  //     sommeNombrePour5 = sommeNombrePour5 + tableauNombresMultpleDe5[i];
+  //   }
+  //   console.log(`La somme des multiples de 3 en dessous de 23 est : ${sommeNombrePour3}`);
+  //   console.log(`La somme des multiples de 5 en dessous de 23 est : ${sommeNombrePour5}`);
+  // }
+
+  // multiple_3_5();
+}
+{
+  //   //Dans un tableau contenant des nombres, lorsuq'on veut par exemple separer les nombres relatifs à deux éléments différents 
+  // // il arrive que le nombre commun soit exclu si l'on utilise la structure if, else if
+  // for(i = 3; i < 23; i++) {
+  //   let nombre = i;
+  //   if(nombre % 3 === 0 || nombre % 5 === 0) {
+  //     tableauNombres.push(i);
+  //   }
+  //   if(nombre % 3 === 0) {
+  //     tableauNombresMultpleDe3.push(i)
+  //   } else if(nombre % 5 === 0) {
+  //     tableauNombresMultpleDe5.push(i)
+  //   } else {
+  //     console.log(`${nombre} n'est ni multiple de 3, ni multiple de 5`);
+  //   }
+  // }
+}
+
+function leapyear(year) {
+
+  if (year % 4 === 0 && year % 400 === 0) {
+    year = true;
+  } else {
+    year = false;
+  }
+  return year;
+}
+
+console.log(leapyear(4000));
