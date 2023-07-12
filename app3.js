bc4e944dbaeec4b9e24ef2259a278ebaec1949a5
Chadrack Mbala <102918698+chadrackmbala@users.noreply.github.com>
2023-07-12 01:15:10 +0100

    Add files via upload

---
+++ app3.js
@@ -0,0 +1,56 @@
+let tableauNombres = [];
+let partie = 'a';
+console.log(tableauNombres);
+
+function nParite(n, parite) {
+    let calcul;
+    return n 
+  }
+
+// if(partie === 'a') {
+//     for(i = 10; i <=21; i++) {
+//         tableauNombres.push(i);
+//     }
+    
+//     let tableauNombrePaire = [];
+//     let tableauNombreImpaire = [];
+    
+//     for(let itterateur = 0; itterateur < tableauNombres.length; itterateur++) {
+//         if(tableauNombres[itterateur] % 2 === 0) {
+//             tableauNombrePaire.push(tableauNombres[itterateur])        
+//         } else {
+//             tableauNombreImpaire.push(tableauNombres[itterateur])
+//         }
+//     }
+    
+//     console.log(tableauNombrePaire);
+//     console.log('limite')
+//     console.log(tableauNombreImpaire);
+    
+//     let sommeDeNombrePaires = 0;
+//     let sommeDeNombreImpaires = 0;
+    
+//     for(let itterateur = 0; itterateur < tableauNombrePaire.length; itterateur++) {
+//         sommeDeNombrePaires = sommeDeNombrePaires + tableauNombrePaire[itterateur];
+//     }
+//     console.log(sommeDeNombrePaires);
+    
+//     for(let itterateur = 0; itterateur < tableauNombreImpaire.length; itterateur++) {
+//         sommeDeNombreImpaires = sommeDeNombreImpaires + tableauNombreImpaire[itterateur];
+//     }
+//     console.log(sommeDeNombreImpaires);
+// } else {
+//     console.log('Nombre inconnu !')
+// }
+
+// console.log('Limite');
+
+// for(let itterateur = 0; itterateur < tableauNombres.length; itterateur++) {
+    
+// }
+
+// if(tableauNombres[2] % 2 === 0) {
+//     console.log(`Nombre ${tableauNombres[2]} est paire !`);
+// } else {
+//             console.log(`Nombre ${tableauNombres[2]} est impaire !`);
+//         }
