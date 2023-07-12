849ad7eff23740c3e98568bf4253019952a8d44b
Chadrack Mbala <102918698+chadrackmbala@users.noreply.github.com>
2023-07-12 01:26:48 +0100

    Add files via upload

--- app3.js
+++ app3.js
@@ -1,56 +1,42 @@
 let tableauNombres = [];
 let partie = 'a';
-console.log(tableauNombres);
 
-function nParite(n, parite) {
-    let calcul;
-    return n 
-  }
+if(partie === 'a') {
+    for(i = 10; i <=21; i++) {
+        tableauNombres.push(i);
+    }
+    
+    let tableauNombrePaire = [];
+    let tableauNombreImpaire = [];
+    
+    for(let itterateur = 0; itterateur < tableauNombres.length; itterateur++) {
+        if(tableauNombres[itterateur] % 2 === 0) {
+            tableauNombrePaire.push(tableauNombres[itterateur])        
+        } else {
+            tableauNombreImpaire.push(tableauNombres[itterateur])
+        }
+    }
 
-// if(partie === 'a') {
-//     for(i = 10; i <=21; i++) {
-//         tableauNombres.push(i);
-//     }
-    
-//     let tableauNombrePaire = [];
-//     let tableauNombreImpaire = [];
-    
-//     for(let itterateur = 0; itterateur < tableauNombres.length; itterateur++) {
-//         if(tableauNombres[itterateur] % 2 === 0) {
-//             tableauNombrePaire.push(tableauNombres[itterateur])        
-//         } else {
-//             tableauNombreImpaire.push(tableauNombres[itterateur])
-//         }
-//     }
-    
-//     console.log(tableauNombrePaire);
-//     console.log('limite')
-//     console.log(tableauNombreImpaire);
-    
-//     let sommeDeNombrePaires = 0;
-//     let sommeDeNombreImpaires = 0;
-    
-//     for(let itterateur = 0; itterateur < tableauNombrePaire.length; itterateur++) {
-//         sommeDeNombrePaires = sommeDeNombrePaires + tableauNombrePaire[itterateur];
-//     }
-//     console.log(sommeDeNombrePaires);
-    
-//     for(let itterateur = 0; itterateur < tableauNombreImpaire.length; itterateur++) {
-//         sommeDeNombreImpaires = sommeDeNombreImpaires + tableauNombreImpaire[itterateur];
-//     }
-//     console.log(sommeDeNombreImpaires);
-// } else {
-//     console.log('Nombre inconnu !')
-// }
+    console.log('Tableau Paire');
 
-// console.log('Limite');
+    console.log(tableauNombrePaire);
 
-// for(let itterateur = 0; itterateur < tableauNombres.length; itterateur++) {
+    console.log('Tableau Impaire');
+
+    console.log(tableauNombreImpaire);
     
-// }
-
-// if(tableauNombres[2] % 2 === 0) {
-//     console.log(`Nombre ${tableauNombres[2]} est paire !`);
-// } else {
-//             console.log(`Nombre ${tableauNombres[2]} est impaire !`);
-//         }
+    let sommeDeNombrePaires = 0;
+    let sommeDeNombreImpaires = 0;
+    
+    for(let itterateur = 0; itterateur < tableauNombrePaire.length; itterateur++) {
+        sommeDeNombrePaires = sommeDeNombrePaires + tableauNombrePaire[itterateur];
+    }
+    console.log(`La somme de 6 premiers nombres paires : ${sommeDeNombrePaires}`);
+    
+    for(let itterateur = 0; itterateur < tableauNombreImpaire.length; itterateur++) {
+        sommeDeNombreImpaires = sommeDeNombreImpaires + tableauNombreImpaire[itterateur];
+    }
+    console.log(`La somme de 6 premiers nombres impaires : ${sommeDeNombreImpaires}`);
+} else {
+    console.log('Nombre inconnu !')
+}
